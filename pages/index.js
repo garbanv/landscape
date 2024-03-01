484bb994d7bd4d655aa2a37aed1ef2fc7da349fc
Alexei Garban <garban.valdeon@gmail.com>
2024-03-01 14:40:07 +0100

    edit

--- pages/index.js
+++ pages/index.js
@@ -184,6 +184,7 @@ export async function getStaticProps(context) {
   }
 
   return {
-    props: { data: { values: cleanNullValues } }
+    props: { data: { values: cleanNullValues } },
+    revalidate: 60, // Revalidate the page every 60 seconds
   };
 }
