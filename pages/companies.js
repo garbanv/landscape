80f98e52cebffee84bce1d70a31535cac6ae32f9
Alexei Garban <garban.valdeon@gmail.com>
2024-03-01 14:27:50 +0100

    edit

--- pages/companies.js
+++ pages/companies.js
@@ -1,36 +1,29 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
-import Layout from "../components/Layout";
-import Head from "next/head";
-import CompanyCard from "../components/CompanyCard";
-import TopBarProgress from "react-topbar-progress-indicator";
-//import { categories,subcategories,categoriesWithSubcategories } from '../utils/categoriesAndSubcategories';
-import Loader from "../components/Loader";
-import { CompanyContext } from "../context/CompanyContext";
+import React, { useContext, useState, useRef } from "react";
 import { newModel } from "../context/data";
-import SearchFilters from "../components/SearchFilters";
+import Layout from "../components/Layout";
+import { CompanyContext } from "../context/CompanyContext";
+import { useRouter } from "next/router";
+import { Tooltip } from "react-tooltip";
+import HomepageSubcategory from "../components/HomepageSubcategory";
+import Modal from "../components/Modal";
+import Methodology from "../components/Methodology";
+import TopBarProgress from "react-topbar-progress-indicator";
+import Meta from "../components/Meta";
+import Hero from "../components/Hero";
+import SubcategoryContainer from "../components/SubcategoryContainer";
+import CategoryContainer from "../components/CategoryContainer";
+import ClusterContainer from "../components/ClusterContainer";
+import Toolbar from "../components/Toolbar";
 
-export default function companiesCards({ data }) {
-
-
+export default function Companies({ data }) {
   const [company, setCompany] = useContext(CompanyContext);
+  const [showModal, setShowModal] = useState(false);
 
-  /*   console.log("data",data) */
-
-
-
+  const [selectedEntity, setSelectedEntity] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [loader, setLoader] = useState(true);
-  const [search, setSearch] = useState("");
-  const [noData, setNoData] = useState(true);
+  const [withZoom, setWithZoom] = useState(false);
 
-  const [liveData, setLiveData] = useState(data.values);
-  const [categoriesList, setCategoriesList] = useState([]);
-  const [sorted, setSorted] = useState(true);
-  const [subcategoryList, setSubcategoryList] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [selectedCluster, setSelectedCluster] = useState("All");
-  const [selectedSubcategory, setSelectedSubcategory] = useState("All");
-
+  const router = useRouter();
 
   TopBarProgress.config({
     barColors: {
@@ -39,247 +32,140 @@ export default function companiesCards({ data }) {
     },
     shadowBlur: 5,
   });
+  // console.log("modal",showModal)
 
-  const handleCompanyName = (text) => {
-    const result = data.values.filter((company, index) =>
-      company.name.toLowerCase().includes(text.toLowerCase())
-    );
-
-    setLiveData(result);
-    if (result.length <= 0) {
-      setNoData(true);
-    } else {
-      setNoData(false);
-    }
+  const handleCompany = (entity) => {
+    setSelectedEntity(entity);
+    setShowModal(true);
   };
 
-  const handleSorted = () => {
+  const [searchResult, setSearchResult] = useState(false);
 
-    setSorted(!sorted);
-    if (sorted) {
-      setLiveData(liveData.sort((a, b) => b.name.localeCompare(a.name)));
-    } 
-    if (!sorted){ 
-      setLiveData(liveData.sort((a, b) => a.name.localeCompare(b.name)));
-    }
+  const handleSearchMessage = () => {
+    setSearchResult(true);
+    setTimeout(() => setSearchResult(false), 3000);
   };
 
-
-
-  const handleFilter = () => {
-    if (company.searchInput !== "") {
-      handleCompanyName(company.searchInput);
-    }
-
-    if (
-      selectedCluster === "All" &&
-      selectedSubcategory === "All" &&
-      selectedCategory === "All"
-    ) {
-      setLiveData(data.values);
-      setSubcategoryList(subcategories);
-    }
-
-    if (
-      selectedCluster !== "All" &&
-      selectedCategory === "All" &&
-      selectedSubcategory === "All"
-    ) {
-      const result = data.values.filter((company, index) =>
-        company.cluster?.includes(selectedCluster)
-      );
-      setLiveData(result);
-      //  setCategoriesList(subcatgeoriesOfSelectedCategory);
-      //  setSubcategoryList(subcatgeoriesOfSelectedCategory[0]?.subcategorxies);
-    }
-
-    if (
-      selectedCluster == "All" &&
-      selectedCategory !== "All" &&
-      selectedSubcategory === "All"
-    ) {
-      const result = data.values.filter((company, index) =>
-        company.category?.includes(selectedCategory)
-      );
-      setLiveData(result);
-      //  setCategoriesList(subcatgeoriesOfSelectedCategory);
-      //  setSubcategoryList(subcatgeoriesOfSelectedCategory[0]?.subcategorxies);
-    }
-
-    if (
-      selectedCluster == "All" &&
-      selectedCategory === "All" &&
-      selectedSubcategory !== "All"
-    ) {
-      
-      const result = data.values.filter((company, index) =>
-        company.subcategory?.includes(selectedSubcategory)
-      );
-      setLiveData(result);
-      //  setCategoriesList(subcatgeoriesOfSelectedCategory);
-      //  setSubcategoryList(subcatgeoriesOfSelectedCategory[0]?.subcategorxies);
-    }
-
-    if (
-      selectedCluster !== "All" &&
-      selectedCategory !== "All" &&
-      selectedSubcategory === "All"
-    ) {
-      
-      const result = data.values.filter(
-        (company, index) =>
-          company.cluster?.includes(selectedCluster) &&
-          company.category.includes(selectedCategory)
-      );
-
-      result.length === 0
-        ? setLiveData({
-            message: "No data with that cluster and category relation",
-          })
-        : setLiveData(result);
-      //  setCategoriesList(subcatgeoriesOfSelectedCategory);
-      //  setSubcategoryList(subcatgeoriesOfSelectedCategory[0]?.subcategorxies);
-    }
-
-
-    if (
-      selectedCluster !== "All" &&
-      selectedCategory !== "All" &&
-      selectedSubcategory !== "All"
-    ) {
-      
-      const result = data.values.filter(
-        (company, index) =>
-          company.cluster?.includes(selectedCluster) &&
-          company.category.includes(selectedCategory) &&
-          company.subcategory.includes(selectedSubcategory) 
-
-      );
-
-      result.length === 0
-        ? setLiveData({
-            message: "No data with that cluster, category or subcategory relation",
-          })
-        : setLiveData(result);
-      //  setCategoriesList(subcatgeoriesOfSelectedCategory);
-      //  setSubcategoryList(subcatgeoriesOfSelectedCategory[0]?.subcategorxies);
-    }
-
-
-  };
-
-  const handleLoading = () => {
+  const handleFoundCompany = (company) => {
     setLoading(!loading);
+    router.push(`/company/${company}`);
   };
 
- 
-/*   useEffect(() => {
-       
-
-      }, [sorted]); */
-
-
-  useEffect(() => {
-    
-    handleFilter();
-
-    setTimeout(function () {
-      setLoader(false);
-    }, 1000);
-  }, [selectedCategory, selectedSubcategory, selectedCluster]);
-
-  const clusters = Object.keys(newModel);
-
-  const categories = Object.entries(newModel)
-    .map(([clusters, values], index) => {
-      const allCategories = [];
-      const data = Object.entries(values.categories).map(
-        ([category, value], i) => {
-          return allCategories.push(category);
-        }
+  const handleSearch = () => {
+    if (company.searchInput !== "") {
+      const result = data.values.filter(
+        (item, index) => item.name.toLowerCase() === company.searchInput
       );
-      return allCategories;
-    })
-    .flat();
-
-  const subcategories = Object.entries(newModel)
-    .map(([clusters, values], index) => {
-      return Object.entries(values.categories)
-        .map(([category, value], i) => {
-          return Object.entries(value.subcategories)
-            .map(([sub, v], ind) => {
-              return v.name;
-            })
-            .flat();
-        })
-        .flat();
-    })
-    .flat();
-
+      result.length === 0
+        ? handleSearchMessage()
+        : handleFoundCompany(result[0].name);
+    }
+  };
 
   return (
     <Layout>
-      <Head>
-        <title>apidays Landscape - Companies</title>
-        <meta name="description" content="apidays landscape companies" />
-      </Head>
       {loading && <TopBarProgress />}
-      <SearchFilters
-        categories={categories}
-        subcategoryList={subcategories}
-        clusters={clusters}
-        // total={total}
-        handleSorted={handleSorted}
-        setSelectedCategory={setSelectedCategory}
-        setSelectedSubcategory={setSelectedSubcategory}
-        handleCompanyName={handleCompanyName}
-        sorted={sorted}
-        selectedCluster={selectedCluster}
-        setSelectedCluster={setSelectedCluster}
-        subcategories={subcategories}
-        datalength={liveData.length}
-      />
+      <Meta />
+      <Hero />
+      <section className="bg-[#083ECB]">
+        <div className="px-2 md:px-6 lg:px-10">
+          <Toolbar
+            setLoading={setLoading}
+            setWithZoom={setWithZoom}
+            data={data}
+          />
+          <Tooltip
+            /*  style={{ backgroundColor: "#083ECB", color: "#fff" }} */
+            textColor="#fff"
+            id="companyName-tooltip"
+          />
+          <Tooltip
+/*             style={{ backgroundColor: "#083ECB", color: "#fff" }} */
+            textColor="#fff"
+            id="category-tooltip"
+            place="right"
+            className="cluster-tooltip"
 
-      {loader && (
-        <div className="container mx-auto text-center flex justify-center my-5">
-          <img src="../Spinner-1s-44px.gif" />{" "}
-        </div>
-      )}
+          />
+          <Tooltip
+         
+/*             style={{ backgroundColor: "#083ECB", color: "#fff" }} */
+            textColor="#fff"
+            id="category-tooltip"
+            place="right"
+            className="category-tooltip"
+          />
+          <Tooltip
+     /*       style={{ backgroundColor: "#083ECB", color: "#fff" }} */
+            textColor="#fff"
+            id="subcategory-tooltip"
+          />
 
-      <section className="cards ">
-        <div className="container mx-auto">
-        {liveData.message ? <p className="font-bold text-center bg-blue-50 rounded-lg px-2 py-1">{liveData.message}</p> : ""}
-          <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 rounded md:px-0 px-5 my-10">
-           
-            {liveData.length > 0 ? (
-              liveData?.map((company, index) => {
-                return (
-                  <CompanyCard
-                    company={company}
-                    index={index}
-                    handleLoading={handleLoading}
-                    key={index}
-                  />
-                )
-              })
-            ) : (
-              
-             <></>
-           
-            )}
-
-            {liveData.length <= 0 && !loader ? "No Data..." : ""}
-
-            {/* {noData ? <h3 className="fw-bold">No Data</h3>: <img src="../waiting.gif"/>}  */}
-          </div>
+          <section className="grid gap-10" id="cluster-container">
+            {Object.entries(newModel)?.map(([clusterName, values], index) => {
+              return (
+                <ClusterContainer
+                  clusterName={clusterName}
+                  values={values}
+                  index={index}
+                  key={index}
+                >
+                  {Object.entries(values?.categories).map(
+                    ([categorieName, values], index) => {
+                      return (
+                        <CategoryContainer
+                          categorieName={categorieName}
+                          index={index}
+                          values={values}
+                          key={index}
+                        >
+                          {values?.subcategories?.map((subcat, index) => {
+                            // if (subcat.name === 'Red Hat') console.log("subcat,", subcat, categorieName);
+                            const filteredSubcategory = data?.values.filter(
+                              (company, index) =>
+                                // company.category?.includes(categorieName) &&
+                                company?.subcategory?.includes(subcat.name)
+                            );
+                            filteredSubcategory.sort((a, b) =>
+                              a.name.localeCompare(b.name)
+                            );
+                            return (
+                              <SubcategoryContainer
+                                subcategory={subcat}
+                                handleCompany={handleCompany}
+                                filteredSubcategory={filteredSubcategory}
+                                withZoom={withZoom}
+                                index={index}
+                                key={index}
+                              />
+                            );
+                          })}
+                        </CategoryContainer>
+                      );
+                    }
+                  )}
+                </ClusterContainer>
+              );
+            })}
+          </section>
         </div>
       </section>
+
+      <Methodology />
+      <Modal
+        selectedEntity={selectedEntity}
+        setLoading={setLoading}
+        setShowModal={setShowModal}
+        showModal={showModal}
+      />
     </Layout>
   );
 }
-
 export async function getStaticProps(context) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v2/companies`, {
+    next:{
+      revalidate:60
+    },
     method: "GET",
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
@@ -298,6 +184,6 @@ export async function getStaticProps(context) {
   }
 
   return {
-    props: { data: { values: cleanNullValues.sort((a, b) => a.name.localeCompare(b.name)) } },
-    revalidate: 60
+    props: { data: { values: cleanNullValues } }
+  };
 }
